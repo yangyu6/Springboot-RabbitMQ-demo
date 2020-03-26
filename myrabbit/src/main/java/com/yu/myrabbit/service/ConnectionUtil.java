7d0d945d6ed693fcbb9b3c639c7afd6cf97b42ed
yu <773024110@qq.com>
2020-03-26 17:13:27 +0800

    初始化项目

--- myrabbit/src/main/java/com/yu/myrabbit/service/ConnectionUtil.java
+++ myrabbit/src/main/java/com/yu/myrabbit/service/ConnectionUtil.java
@@ -9,7 +9,7 @@ public class ConnectionUtil {
         //定义连接工厂
         ConnectionFactory factory = new ConnectionFactory();
         //设置服务地址
-        factory.setHost("47.114.161.233");
+        factory.setHost("locahost");
         //端口
         factory.setPort(5672);
         //设置账号信息，用户名、密码、vhost
